3a326d4ddac693e50c8ce41667197c251d4b5343
lucas <lucas.devtibetano@gmail.com>
2025-07-01 10:31:24 -0300

    First commit.

---
+++ src/test/java/com/api/anigame/AnigameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.anigame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnigameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
