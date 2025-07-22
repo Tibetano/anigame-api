f3c2348ef37d3e787dca79c6c20f46a66a3f2a54
lucas <lucas.devtibetano@gmail.com>
2025-07-22 11:05:49 -0300

    Update: changed request attribute names from CamelCase pattern to Snake_case pattern.

--- src/main/java/com/api/anigame/dto/RefreshTokenReqDTO.java
+++ src/main/java/com/api/anigame/dto/RefreshTokenReqDTO.java
@@ -1,6 +1,9 @@
 package com.api.anigame.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record RefreshTokenReqDTO(
+        @JsonProperty("refresh_token")
         String refreshToken
 ) {
 }
--- src/main/java/com/api/anigame/dto/UserCredentialsResDTO.java
+++ src/main/java/com/api/anigame/dto/UserCredentialsResDTO.java
@@ -1,8 +1,13 @@
 package com.api.anigame.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record UserCredentialsResDTO(
+        @JsonProperty("access_token")
         String accessToken,
+        @JsonProperty("refresh_token")
         String refreshToken,
+        @JsonProperty("expires_in")
         Long expiresIn
 ) {
 }
